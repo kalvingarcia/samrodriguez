9d13e28bbfdc45038673ba2bba10d5478e7e8833
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-10 13:45:13 -0400

    begun styling the header and footer

--- source/components/header.jsx
+++ source/components/header.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import {tss} from './common/theme';
 
 const useStyles = tss.create(({theme}) => ({
+    header: {
+        padding: "20px",
+        width: "100%",
+        height: "50px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        overflow: "hidden",
 
+        "&:hover": {
+            backgroundColor: theme.neutral.containerLowest.hex()
+        }
+    },
+    navlinks: {
+        display: "flex",
+        alignItems: "center",
+        gap: "20px"
+    }
 }));
 
 export default function Header({}) {
@@ -10,8 +27,9 @@ export default function Header({}) {
     return (
         <header className={classes.header}>
             <span>Sam Rodriguez</span>
-            <div>
+            <div className={classes.navlinks}>
                 <span>Work</span>
+                <span>About</span>
                 <span>Contact</span>
             </div>
         </header>
