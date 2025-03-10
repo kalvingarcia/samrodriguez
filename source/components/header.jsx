6be19cbba399c393438da058de108ace00a9b209
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-10 15:37:58 -0400

    adding padding to the header

--- source/components/header.jsx
+++ source/components/header.jsx
@@ -6,16 +6,20 @@ import {Label} from './common/typography';
 const useStyles = tss.create(({theme, scrolled, hovered}) => ({
     header: {
         position: "sticky",
+        padding: "0px 40px",
         top: 0,
         zIndex: 1000,
-        padding: "20px",
+        backgroundColor: scrolled || hovered? theme.neutral.containerLowest.hex() : theme.primary.container.hex(),
+    },
+    content: {
+        margin: "auto",
         width: "100%",
+        maxWidth: "1000px",
         height: "50px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
         overflow: "hidden",
-        backgroundColor: scrolled || hovered? theme.neutral.containerLowest.hex() : theme.primary.container.hex(),
     },
     navlinks: {
         display: "flex",
@@ -42,11 +46,13 @@ export default function Header({}) {
     return (
         <Container role={scrolled || hovered? "neutral" : "primary"} type="container">
             <header className={classes.header} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-                <Label>Sam Rodriguez</Label>
-                <div className={classes.navlinks}>
-                    <Label>Work</Label>
-                    <Label>About</Label>
-                    <Label>Contact</Label>
+                <div className={classes.content}>
+                    <Label>Sam Rodriguez</Label>
+                    <div className={classes.navlinks}>
+                        <Label>Work</Label>
+                        <Label>About</Label>
+                        <Label>Contact</Label>
+                    </div>
                 </div>
             </header>
         </Container>
