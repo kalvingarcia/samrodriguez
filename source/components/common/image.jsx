373d57ba3aef22c430210250c75caeb19f522b8a
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-18 15:27:14 -0400

    added image component

---
+++ source/components/common/image.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {tss} from './theme';
+
+const useStyles = tss.create(({theme}) => ({
+    image: {
+        overflow: "hidden",
+
+        "& img": {
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center"
+        }
+    }
+}));
+
+export default function Image({className, source, alt}) {
+    const {cx, classes} = useStyles();
+    return (
+        <figure className={cx(classes.image, className?? "")}>
+            <img src={source} alt={alt} />
+        </figure>
+    );
+}
