e279c8dc5973fa00b814bde6048d19711a0ff1e5
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-18 15:27:22 -0400

    added profile image

--- source/content/about.jsx
+++ source/content/about.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {tss} from '../components/common/theme';
 import {Body, Display} from '../components/common/typography';
+import Button from '../components/common/button';
+import Image from '../components/common/image';
+import profile from '../../public/images/profile.jpg';
 
 const useStyles = tss.create(({theme}) => ({
     section: {
@@ -11,7 +14,7 @@ const useStyles = tss.create(({theme}) => ({
         padding: "40px",
         width: "100%",
         height: "500px",
-        backgroundColor: theme.primary.container.hex()
+        backgroundColor: theme.neutral.containerHighest.hex()
     },
     container: {
         margin: "auto",
@@ -24,15 +27,18 @@ const useStyles = tss.create(({theme}) => ({
     wave: {
         width: "100%",
         height: "200px",
-        fill: theme.primary.container.hex()
+        fill: theme.neutral.containerHighest.hex()
     },
     image: {
         width: "100%",
         height: "400px",
-        backgroundColor: theme.secondary.accent.hex()
+        border: `3pt dashed ${theme.primary.accent.hex()}`
     },
     bio: {
         width: "100%",
+    },
+    intro: {
+        color: theme.secondary.accent.hex()
     }
 }));
 
@@ -42,15 +48,16 @@ export default function About({}) {
         <section id="about" className={classes.section}>
             <div className={classes.content}>
                 <div className={classes.container}>
-                    <div className={classes.image} />
+                    <Image className={classes.image} source={profile} alt="Portraiit of Sam" />
                     <div className={classes.bio}>
-                        <Display>Hi, I'm Sam!</Display>
-                        <Body>I'm a graphic designer who is passionate about branding, packaging, and motion graphics.</Body>
+                        <Display className={classes.intro}>hi, i'm sam!</Display>
+                        <Body>i'm a graphic designer who is passionate about branding, packaging, and motion graphics.</Body>
+                        <Button>learn more!</Button>
                     </div>
                 </div>
             </div>
             <svg className={classes.wave} xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink" preserveAspectRatio='none' viewBox="0 0 900 300" version="1.1">
-                <path xmlns="http://www.w3.org/2000/svg" d="M0 192L50 207C100 222 200 252 300 242.5C400 233 500 184 600 161.3C700 138.7 800 142.3 850 144.2L900 146L900 0L850 0C800 0 700 0 600 0C500 0 400 0 300 0C200 0 100 0 50 0L0 0Z" stroke-linecap="round" stroke-linejoin="miter"/>
+                <path xmlns="http://www.w3.org/2000/svg" d="M0 192L50 207C100 222 200 252 300 242.5C400 233 500 184 600 161.3C700 138.7 800 142.3 850 144.2L900 146L900 0L850 0C800 0 700 0 600 0C500 0 400 0 300 0C200 0 100 0 50 0L0 0Z" strokeLinecap="round" strokeLinejoin="miter"/>
             </svg>
         </section>
     );
