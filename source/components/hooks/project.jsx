202834f57c0837de6bbd0a29b2bc7837b5b25cdc
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-19 15:59:19 -0400

    created search param navigation for projects

--- source/components/hooks/project.jsx
+++ source/components/hooks/project.jsx
@@ -1,26 +1,39 @@
-"use client"
-import React, {createContext, useContext} from "react"
+import {useEffect, useState} from 'react';
 
-const ProjectContext = createContext({});
+export default function useProject() {
+    const getProjectFromURL = () => new URL(window.location.href).searchParams.get("open")?? undefined;
+    const [project, setProject] = useState(getProjectFromURL);
 
-export default function useProjectContext() {
-    const getOpenProject = () => {
-
-    };
-    const toggleProject = () => {
-
-    };
-
-    const projectContext = () => {
-        return useContext(ProjectContext);
-    };
-    const ProjectProvider = ({children}) => {
-        return (
-            <ProjectContext.Provider value={{getOpenProject, toggleProject}}>
-                {children}
-            </ProjectContext.Provider>
-        );
+    const updateURL = (directory) => {
+        const url = new URL(window.location.href);
+        if (directory) {
+            url.searchParams.set("open", directory);
+        } else {
+            url.searchParams.delete("open");
+        }
+        window.history.pushState({}, "", url);
+        
+        // Manually trigger a state update
+        window.dispatchEvent(new Event("pushstate"));
     };
 
-    return {projectContext, ProjectProvider};
+    useEffect(() => {
+        const handleLocationChange = () => {
+            setProject(getProjectFromURL()); // Update state with latest URL
+        };
+
+        window.addEventListener("popstate", handleLocationChange);
+        window.addEventListener("pushstate", handleLocationChange);
+
+        return () => {
+            window.removeEventListener("popstate", handleLocationChange);
+            window.removeEventListener("pushstate", handleLocationChange);
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("Project updated:", project);
+    }, [project]);
+
+    return {project, openProject: updateURL, closeProject: () => updateURL(undefined)};
 }
