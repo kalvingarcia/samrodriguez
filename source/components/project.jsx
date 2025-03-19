723f7f5ab104f21bae95049ce7b3d1c737a91bfc
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-18 22:12:18 -0400

    added projects json and project auto sizing by window size

--- source/components/project.jsx
+++ source/components/project.jsx
@@ -2,9 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {tss} from './common/theme';
 import Image from '../components/common/image';
 
-const useStyles = tss.create(({theme}) => ({
+const useStyles = tss.create(({theme, contentSize}) => ({
     project: {
-        width: "100%",
+        width: "contentSize",
         height: "auto",
         aspectRatio: 1,
         backgroundColor: theme.neutral.containerLowest.hex()
@@ -18,15 +18,26 @@ const useStyles = tss.create(({theme}) => ({
 export default function Project({directory}) {
     const [loading, setLoading] = useState(true);
     const [image, setImage] = useState(undefined);
+    const [contentSize, setContentSize] = useState();
     useEffect(() => {
         (async () => {
             setLoading(true);
             setImage((await import(`../../public/images/projects/${directory}/thumbnail.jpg`)).default);
             setLoading(false);
         })();
+
+        const checkWindowSize = () => {
+            const width = Math.min(1000, window.innerWidth);
+            setContentSize(width / 3);
+            console.log(width / 3);
+        };
+        checkWindowSize();
+
+        window.addEventListener("resize", checkWindowSize);
+        return () => window.removeEventListener("resize", checkWindowSize);
     }, []);
 
-    const {classes} = useStyles({})
+    const {classes} = useStyles({contentSize})
     return (
         <div className={classes.project}>
             {!loading && image && <Image className={classes.thumbnail} source={image} alt="Project link for opening project info." />}
