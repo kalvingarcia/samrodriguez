856b69da465d6053fc9f9e85adc6d3747ba77432
Kalvin Garcia <kalvigarcia@gmail.com>
2025-03-18 22:08:48 -0400

    added darkmode button

--- source/components/header.jsx
+++ source/components/header.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import {tss} from './common/theme';
+import {tss, useDarkMode} from './common/theme';
 import useContainer from './common/hooks/container';
 import {Label} from './common/typography';
-import {Icon} from './common/iconography';
+import IconButton, {Icon} from './common/iconography';
 
 const useStyles = tss.create(({theme, scrolled, hovered}) => ({
     header: {
@@ -16,7 +16,7 @@ const useStyles = tss.create(({theme, scrolled, hovered}) => ({
         margin: "auto",
         width: "100%",
         maxWidth: "1000px",
-        height: "50px",
+        height: "75px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
@@ -24,7 +24,21 @@ const useStyles = tss.create(({theme, scrolled, hovered}) => ({
     },
     logo: {
         display: "flex",
-        gap: "10px"
+        alignItems: "center",
+
+        "& .icon": {
+            fontSize: "50px"
+        },
+        "& .text": {
+            fontFamily: "var(--display-font)",
+            fontSize: "1.25rem",
+            marginLeft: "-20px",
+            marginTop: "-5px",
+
+            "@media (max-width: 1080px)": {
+                display: "none"
+            }
+        }
     },
     navlinks: {
         display: "flex",
@@ -34,6 +48,8 @@ const useStyles = tss.create(({theme, scrolled, hovered}) => ({
 }));
 
 export default function Header({}) {
+    const {darkMode, toggleDarkMode} = useDarkMode();
+
     const [hovered, setHovered] = useState(false);
     const [scrolled, setScrolled] = useState(false);
     const checkScroll = ({target}) => {
@@ -53,13 +69,14 @@ export default function Header({}) {
             <header className={classes.header} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
                 <div className={classes.content}>
                     <div className={classes.logo}>
-                        <Icon iconClass="sam-icons" icon="logo" />
-                        <Label><a href="#about">sam rodriguez</a></Label>
+                        <Icon className="icon" iconClass="sam-icons" icon="logo" />
+                        <Label className="text">am rodriguez</Label>
                     </div>
                     <div className={classes.navlinks}>
                         <Label><a href="#about">about</a></Label>
                         <Label><a href="#projects">work</a></Label>
                         <Label><a href="#contact">contact</a></Label>
+                        <IconButton icon={darkMode? "dark_mode" : "light_mode"} role="tertiary" appearance='tonal' onClick={toggleDarkMode} />
                     </div>
                 </div>
             </header>
