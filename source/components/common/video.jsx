55ae74df62984148d5f62aba8312af6232835f97
Kalvin Garcia <kalvigarcia@gmail.com>
2025-08-13 01:14:19 -0400

    creating a blob from the video file to see if it works

--- source/components/common/video.jsx
+++ source/components/common/video.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { useEffect, useState } from 'react';
 import {tss} from './theme';
 
 const useStyles = tss.create(({theme}) => ({
@@ -10,10 +11,21 @@ const useStyles = tss.create(({theme}) => ({
 }));
 
 export default function Image({className, source, ...props}) {
+    const [sourceRef, setSourceRef] = useState(undefined);
+    useEffect(() => {
+        (async () => {
+            if(sourceRef) {
+                const blob = await fetch(source).then(response => response.blob());
+                console.log(blob)
+                sourceRef.src = URL.createObjectURL(blob);
+            }
+        })();
+    }, [sourceRef]);
+
     const {cx, classes} = useStyles();
     return (
-        <video className={cx(classes.video, className)} {...props}>
-            <source src={source} type="video/mp4" />
+        <video ref={setSourceRef} className={cx(classes.video, className)} {...props}>
+            {/* <source ref={setSourceRef} type="video/mp4" /> */}
             Your browser does not support the video tag.
         </video>
     );
